d657f86c924b81b133ee21cbb68e4b772d7b461c
kallyfuu <fe.olavarria@duocuc.cl>
2024-12-02 22:39:19 -0300

    autenticacion guard

--- arteapp/src/app/guards/autenticacion.guard.ts
+++ arteapp/src/app/guards/autenticacion.guard.ts
@@ -1,12 +1,29 @@
-import { CanActivateFn, CanActivate, ActivatedRouteSnapshot, GuardResult, MaybeAsync, RouterStateSnapshot } from '@angular/router';
-import { Router } from '@angular/router'
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { localStorageService } from '../services/storagesql.service';
 
+@Injectable({
+  providedIn: 'root',
+})
 export class AutenticacionGuard implements CanActivate {
-  constructor(private router: Router) {}
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): MaybeAsync<GuardResult> {
-    throw new Error('Method not implemented.');
+  constructor(private local: localStorageService, private router: Router) {}
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+    const currentUser = this.local.getUser();
+
+    if (currentUser) {
+      if (currentUser.role === 'admin') {
+
+        return true;
+      } else {
+
+        this.router.navigateByUrl('/home');
+        return false;
+      }
+    }
+
+
+    this.router.navigateByUrl('/login');
+    return false;
   }
-
-
-  
 }
